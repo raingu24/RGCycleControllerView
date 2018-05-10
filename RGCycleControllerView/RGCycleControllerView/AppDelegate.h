0b92ff4c3612ae116bdd24d8be196d1e795f96f0
raingu24 <raingu24@163.com>
2018-05-10 19:16:06 +0800

    first commit

---
+++ RGCycleControllerView/RGCycleControllerView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RGCycleControllerView
+//
+//  Created by raingu24 on 2018/5/10.
+//  Copyright © 2018年 raingu24. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RGCycleControllerView/RGCycleControllerView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RGCycleControllerView
+//
+//  Created by raingu24 on 2018/5/10.
+//  Copyright © 2018年 raingu24. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
